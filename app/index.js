459c20bf162c8fb362044cb2ef21d9dfb1c947c5
Swargaraj Bhowmik <swargarajbhowmik@gmail.com>
2024-06-16 23:29:13 +0530

    text reveal animation added

--- app/index.js
+++ app/index.js
@@ -1,1 +1,22 @@
 import "../styles/index.scss";
+import Lenis from "lenis";
+import TextReveal from "./animations/TextReveal";
+
+class App {
+  constructor() {
+    this._createTextReveals();
+  }
+
+  _createTextReveals() {
+    const textItems = [
+      ...document.querySelectorAll('[data-animation="text-reveal"]'),
+    ];
+    textItems.forEach((text, index) => {
+      setTimeout(() => {
+        new TextReveal({ element: text });
+      }, 300 * index);
+    });
+  }
+}
+
+new App();
