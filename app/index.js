884e7074ada14b2e7cfa0d8d9e439944fa9196bd
Swargaraj Bhowmik <swargarajbhowmik@gmail.com>
2024-06-17 12:24:02 +0530

    did some stuffs

--- app/index.js
+++ app/index.js
@@ -1,10 +1,20 @@
 import "../styles/index.scss";
 import Lenis from "lenis";
 import TextReveal from "./animations/TextReveal";
+import ButtonReveal from "./animations/ButtonReveal";
 
 class App {
   constructor() {
+    this._createLenis();
     this._createTextReveals();
+    this._createButtonReveals();
+    this._render();
+  }
+
+  _createLenis() {
+    this.lenis = new Lenis({
+      lerp: 0.07,
+    });
   }
 
   _createTextReveals() {
@@ -16,6 +26,22 @@ class App {
         new TextReveal({ element: text });
       }, 300 * index);
     });
+  }
+
+  _createButtonReveals() {
+    const buttonItems = [
+      ...document.querySelectorAll('[data-animation="button-reveal"]'),
+    ];
+    buttonItems.forEach((button, index) => {
+      setTimeout(() => {
+        new ButtonReveal({ element: button });
+      }, 200 * index);
+    });
+  }
+
+  _render(time) {
+    this.lenis.raf(time);
+    requestAnimationFrame(this._render.bind(this));
   }
 }
 
